662c28cb738c130f34aa1a38fecdf2f79fbaf4e6
Aleicer <aleicer.vesga@gmail.com>
2022-10-05 21:30:49 -0500

    agregacion de pago en vehiculo

--- ticket/models.py
+++ ticket/models.py
@@ -3,7 +3,7 @@ from vehiculo.models import Vehiculo
 
 class Ticket (models.Model):
     tarifa = models.IntegerField()
-    hora_entrada = models.DateTimeField(blank=True, null=False)
+    hora_entrada = models.DateTimeField(blank=True, null=True)
     hora_salida = models.DateTimeField(blank=True, null=True)
     idVehiculo = models.ForeignKey(Vehiculo, null=True, on_delete= models.CASCADE)
     total_pagar = models.IntegerField()
